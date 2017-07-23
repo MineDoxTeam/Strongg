4ab57bce7cee8ef682361224aae5b489bcf90ed7
GuyWhoDoThings <oslavsky.jan@seznam.cz>
2017-07-23 18:47:51 +0200

    Update serversocket.py

--- strongg/serversocket.py
+++ strongg/serversocket.py
@@ -1,1 +1,20 @@
+import sys
+import socket
 
+class ServerSocket:
+  
+  def __init__(self, host, port):
+    self.sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.PORT = port
+    self.ADRESS = host
+    try:
+            self.sck.bind((self.HOST, self.PORT))
+    except self.sck.error:
+            print("Error! Perhaps server is running on this port!")
+    finally:
+            self.sck.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.sck.listen(5)
+            threading.Thread(target=self.listenPackets, daemon=True).start()
+            
+    def listenPackets(self):
+      print("Listening:D")
