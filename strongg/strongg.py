a28dae6f277226ccb3ccc43406516ec0edc6dca7
GuyWhoDoThings <oslavsky.jan@seznam.cz>
2017-07-23 18:35:32 +0200

    Update strongg.py

--- strongg/strongg.py
+++ strongg/strongg.py
@@ -1,1 +1,16 @@
+import os
+import sys
 
+class Strongg:
+  
+  def __init__(self, ip, port):
+    self.PORT = 19132
+    self.STARTED = true
+    self.SERVER_VERSION = "1.1"
+    self.ADRESS = "localhost"
+    self.startAll()
+    
+  def startAll():
+    print("Starting Strongg server on ip " + self.ADRESS + ":" + self.PORT);
+    self.sck = ServerSocket(self.ADRESS, self.PORT)
+    
