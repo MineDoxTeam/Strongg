edbf2d549270cd2df5cae0ea7a615354a982aff0
GuyWhoDoThings <oslavsky.jan@seznam.cz>
2017-07-23 17:31:10 +0200

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,1 +1,13 @@
-
+import sys
+import os
+import platform
+if platform.system() == "Linux":
+  print("Strongg does not support Linux Devices!")
+  
+  
+if sys.version_info > (3,2):
+  print("Update your Python version to version 3.2 or higher!")
+  print("You can download it on http://python.org/downloads")
+elif platform.system() == "Windows":
+  //Construct Strongg class
+   
